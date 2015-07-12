3f1935b92c7e4117f77b66a5993eaeb90125b6fc
Naoki Takano <honten@tinkermode.com>
2015-07-11 17:34:36 -0700

    White header

--- Example/MODE-iOSSDK/MODEAppDelegate.m
+++ Example/MODE-iOSSDK/MODEAppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     // Override point for customization after application launch.
     [[DataHolder sharedInstance] loadData];
     return YES;
